6538a0eb039a3b335ba78fe170d6aa4a094c0969
Julian von Selasinsky <julian.vonselasinsky@outlook.de>
2025-11-20 09:52:00 +0100

    update version

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const FactoryManagerName = 'Factory Manager';
-export const FactoryManagerVersion = '1.5.10';
+export const FactoryManagerVersion = '1.5.9';
