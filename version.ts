3a23451bbbf913756040734dec79bd81ebc5431e
Sela1236 <sela123@gmx.de>
2026-04-28 10:32:50 +0200

    Translation of Storable Members

--- version.ts
+++ version.ts
@@ -16,4 +16,4 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  */
 export const FactoryManagerName = 'Factory Manager';
-export const FactoryManagerVersion = '1.6.2';
+export const FactoryManagerVersion = '1.6.3';
